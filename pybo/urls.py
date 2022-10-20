42dc9d7a31ef5129ecce3693785dc55fdab8f880
kmjang <kyumin.jang@gmail.com>
2022-10-20 16:47:54 +0900

    url 별칭

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,8 +1,14 @@
+"""
+    app_name : templates에서 url을 지칭하기 위해서 사용, redirect 함수에서도 사용 가능함
+    path 함수안에 name은 templates에서 url 호출 시 사용
+"""
 from django.urls import path, include
 
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
